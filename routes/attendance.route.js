97d85fa3e7559b37af5a8d01b823892d361433f1
fajarKurniawaans <fk357421@gmail.com>
2025-11-04 12:14:47 +0700

    ChangesAttendanceRouteHandler

--- routes/attendance.route.js
+++ routes/attendance.route.js
@@ -3,7 +3,6 @@ import {
   postAttendance,
   getAttendanceByID,
   getSummaryMothlyAttendance,
-  postAnalysisAttendance,
 } from "../controllers/attendance.controller.js";
 
 const app = express();
